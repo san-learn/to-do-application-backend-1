c6028ce8fdb108aa2763bc5a9aa7f82c2203a3df
San Learn <sanlearn3@gmail.com>
2025-01-27 12:16:17 +0700

    fix: update middleware usage to match variable name
    - Change `app.use` to `application.use` to align with the variable name `application`

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 application.use(express.static(path.join(__dirname, "../public")));
-app.use(
+application.use(
   cors({
     origin: (origin, callback) => {
       const allowedOrigins = [
